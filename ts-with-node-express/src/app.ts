1714d1b533fb6851055aa8c3995f52624f231465
Kris Renaldi <krenaldi@comcast.net>
2020-05-29 19:58:29 -0400

    Using TS with Node Express

---
+++ ts-with-node-express/src/app.ts
@@ -0,0 +1,16 @@
+import express, { json, Request, Response, NextFunction } from 'express';
+
+import todoRoutes from './routes/todos';
+
+const app = express();
+
+app.use(json());
+
+app.use('/todos', todoRoutes);
+
+// Middleware function
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).json({ message: err.message });
+})
+
+app.listen(3000);
---
+++ ts-with-node-express/src/routes/todos.ts
@@ -0,0 +1,17 @@
+// const express = require('express');
+// const router = express.Router();
+import { Router } from 'express';
+
+import { createTodo, getTodos, updateTodos, deleteTodo } from '../controllers/todos';
+
+const router = Router();
+
+router.post('/', createTodo);
+
+router.get('/', getTodos);
+
+router.patch('/:id', updateTodos);
+
+router.delete('/:id', deleteTodo);
+
+export default router;
